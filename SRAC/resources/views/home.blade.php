4186f79cadee1305648c3c419db18b7669d177af
Camilo Oviedo Fraga <camilo.oviedof@gmail.com>
2015-11-10 12:30:51 -0300

    ADD-avance-vistas

--- SRAC/resources/views/home.blade.php
+++ SRAC/resources/views/home.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.principal')
 
+@section('styles')
+
+    @endsection
 @section('navegador')
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
     @include('partials.nav')
 @endsection
 
@@ -10,17 +14,22 @@
     <p>
         informacion del club
     </p>
-    <div class="form-group">
+    <div class="form-group" id="formulario-registro">
+    <h4>Ingresar Cuenta</h4>
+        <hr>
     <form action="login" method="post" class="form-horizontal">
         <label class="control-label" for="usuario">@lang('auth.user')</label>
         <input type="text" name="textUsuario" id="TextUsuario">
+        <p></p>
         <label class="control-label" for="password">@lang('auth.password')</label>
         <input type="password" name="password" id="password">
+        <p></p>
         <button class="btn-primary" type="submit">@lang('auth.login')</button>
+
     </form>
-    </div>
-    <div>
-        Aun no tienes tu cuenta? <a href="{{Route('usuario.create')}}">@lang('auth.register')</a>
+        <p></p>
+        ¿Aun no tienes tu cuenta? <a href="{{Route('usuario.create')}}">@lang('auth.register')</a>
+
     </div>
 
 @endsection
